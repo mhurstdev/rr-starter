75444a5283ead8f7d87316d8062d9f692d026a40
Martin Hurst <contact@martinhurst.com>
2026-04-18 18:58:00 +0100

    chore: add eslint + rule packages

---
+++ eslint.config.ts
@@ -0,0 +1,52 @@
+import js from '@eslint/js';
+import _import from 'eslint-plugin-import';
+import jsxa11y from 'eslint-plugin-jsx-a11y';
+import reactHooks from 'eslint-plugin-react-hooks';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+	{
+		ignores: ['build/**', 'node_modules/**'],
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+				...globals.es2021,
+			},
+		},
+	},
+	js.configs.recommended,
+	{
+		files: ['**/*.{ts,tsx}'],
+		extends: [
+			tseslint.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			jsxa11y.flatConfigs.recommended,
+		],
+		plugins: {
+			import: _import,
+		},
+		settings: {
+			'import/resolver': {
+				typescript: true,
+				node: true,
+			},
+		},
+		rules: {
+			'import/no-unresolved': 'error',
+			'@typescript-eslint/consistent-type-imports': [
+				'error',
+				{
+					prefer: 'type-imports',
+					fixStyle: 'separate-type-imports',
+				},
+			],
+			'import/consistent-type-specifier-style': [
+				'error',
+				'prefer-top-level',
+			],
+		},
+	},
+]);
