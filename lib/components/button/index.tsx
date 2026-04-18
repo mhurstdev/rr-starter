d14e9696403f7b15c99408f2a99db3b0c6da70ab
Martin Hurst <contact@martinhurst.com>
2026-04-18 19:41:33 +0100

    chore: add css modules file to example component

--- lib/components/button/index.tsx
+++ lib/components/button/index.tsx
@@ -1,8 +1,14 @@
+import { clsx } from 'clsx';
+import styles from './index.module.css';
 import type * as React from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-export function Button({ children, ...restProps }: ButtonProps) {
-	return <button {...restProps}>{children}</button>;
+export function Button({ className, children, ...restProps }: ButtonProps) {
+	return (
+		<button className={clsx(styles.base, className)} {...restProps}>
+			{children}
+		</button>
+	);
 }
