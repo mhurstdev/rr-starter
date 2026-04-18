52f862853736aebcb5726057a087f7da659cf722
Martin Hurst <contact@martinhurst.com>
2026-04-18 18:59:44 +0100

    chore: format

--- app/env.client.ts
+++ app/env.client.ts
@@ -1,11 +1,11 @@
-import * as z from "zod";
-import { createEnv } from "@t3-oss/env-core";
+import { createEnv } from '@t3-oss/env-core';
+import * as z from 'zod';
 
 export const env = createEnv({
-  clientPrefix: "VITE_",
-  client: {
-    VITE_CLIENT_EXAMPLE: z.string().min(1),
-  },
-  runtimeEnv: import.meta.env,
-  emptyStringAsUndefined: true,
+	clientPrefix: 'VITE_',
+	client: {
+		VITE_CLIENT_EXAMPLE: z.string().min(1),
+	},
+	runtimeEnv: import.meta.env,
+	emptyStringAsUndefined: true,
 });
--- app/env.server.ts
+++ app/env.server.ts
@@ -1,10 +1,10 @@
-import * as z from "zod";
-import { createEnv } from "@t3-oss/env-core";
+import { createEnv } from '@t3-oss/env-core';
+import * as z from 'zod';
 
 export const env = createEnv({
-  server: {
-    SERVER_EXAMPLE: z.string().min(1),
-  },
-  runtimeEnv: process.env,
-  emptyStringAsUndefined: true,
+	server: {
+		SERVER_EXAMPLE: z.string().min(1),
+	},
+	runtimeEnv: process.env,
+	emptyStringAsUndefined: true,
 });
