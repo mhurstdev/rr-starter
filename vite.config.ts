8877c99b52db3fe7526d1c09ede290539b69b0b6
Martin Hurst <contact@martinhurst.com>
2026-04-25 11:44:46 +0100

    fix: stop nitro causing vitest to hang

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,28 @@ import viteReact from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
 
+function getPlugins() {
+	if (process.env.VITEST) {
+		return [viteReact()];
+	}
+
+	return [
+		devtools(),
+		nitro({
+			output: {
+				dir: 'build',
+			},
+		}),
+		tanstackStart(),
+		viteReact(),
+	];
+}
+
 export default defineConfig({
 	resolve: {
 		tsconfigPaths: true,
 	},
-	plugins: [
-		devtools(),
-		nitro({ output: { dir: 'build' } }),
-		!process.env.VITEST && tanstackStart(),
-		viteReact(),
-	],
+	plugins: getPlugins(),
 	test: {
 		environment: 'jsdom',
 		globals: true,
